dc95c267d928e6565050c98e8eb53519bf073338
Noor A <nooraldinahmed@gmail.com>
2023-09-01 12:00:27 +0600

    file logger added

--- website/settings/production.py
+++ website/settings/production.py
@@ -7,6 +7,42 @@ load_dotenv()
 
 DEBUG = False
 
+# LOGGING
+LOG_DIR = os.path.join(BASE_DIR, 'logs')
+LOG_FILE = '/debug.log'
+LOG_PATH = LOG_DIR + LOG_FILE
+
+if not os.path.exists(LOG_DIR):
+    os.mkdir(LOG_DIR)
+
+if not os.path.exists(LOG_PATH):
+    f = open(LOG_PATH, 'a').close() #create empty log file
+else:
+    f = open(LOG_PATH, 'w').close()
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": LOG_PATH,
+        },
+    },
+    "root": {
+        "handlers": ["file"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+        },
+    },
+}
+
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", '*').split(",")
